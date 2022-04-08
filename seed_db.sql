a0fbb51af5feb37d59f5801c195801076396e2fe
Boris Vassilev <boris.n.vassilev@gmail.com>
2022-04-08 14:09:34 +0300

    fixed: prices had only one decimal digit

--- seed_db.sql
+++ seed_db.sql
@@ -16,7 +16,7 @@ CREATE TABLE shop.products (
 	ProductID		INT				NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	Name			NVARCHAR(40)	NOT NULL,
 	Description		VARCHAR(200)	NOT NULL,
-	Price			FLOAT(10, 1)	NOT NULL,
+	Price			FLOAT(10, 2)	NOT NULL,
 	CurrentQuantity INT				UNSIGNED DEFAULT 0
 );
 
@@ -35,13 +35,13 @@ INSERT INTO shop.users (UserName, PasswordHash, FirstName, LastName, Email, Perm
 ("SCP-2770-1"	, SHA2("I think you forgot about the water table, you fucking idiot."	, 256), "1"		, "2770", "jo@hn.com"					, 0);
 
 
-INSERT INTO shop.products (Name, Description, Price, CurrentQuantity) VALUES 
-("печка"				, "пе4ка"													, 420.0 , 0),
-("лестаро телевизор"	, "телевизор леново, ама стар"								, 1337.0, 0),
-("ютия"					, "lorem Ipsum dolor sit amet, conseqteur adipiscing elit"	, 69.0  , 0),
-("кабел"				, "кабел (надъвкан)"										, 10.0  , 0),
-("митковълнова печка"	, "ММММММММММММММММММММММММММММММММММММММММММММММММММММММММ", 119.0 , 0),
-("тостер"				, "Аз съм тостер"											, 426.0 , 0);
+INSERT INTO shop.products (Name, Description, Price) VALUES 
+("печка"				, "пе4ка"													, 420.0 ),
+("лестаро телевизор"	, "телевизор леново, ама стар"								, 1337.0),
+("ютия"					, "lorem Ipsum dolor sit amet, conseqteur adipiscing elit"	, 69.0  ),
+("кабел"				, "кабел (надъвкан)"										, 10.0  ),
+("митковълнова печка"	, "ММММММММММММММММММММММММММММММММММММММММММММММММММММММММ", 119.0 ),
+("тостер"				, "Аз съм тостер"											, 426.0 );
 
 
 SELECT * FROM shop.users;
